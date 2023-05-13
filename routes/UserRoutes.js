77dfbd342177f48d73422aeb68c101118617344e
Akhil-5292 <aps.ca9@gmail.com>
2023-05-13 13:45:05 +0530

    Api

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -10,6 +10,6 @@ const {
   router.post("/add", addToLikedMovies);
   router.put("/remove", removeFromLikedMovies);
 
-  router.all("/*", function (req, res) { res.status(404).send({ status: false, msg: "Invalid HTTP request" }) })
+  router.all("/*", function (req, res) { res.status(404).send({ status: false, msg: "Invalid HTTP request" }) });
   
   module.exports = router;
